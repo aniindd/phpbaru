78153b66b2183507fcfbda70bcb79ebc1ff36abd
aniindd <112366186+aniindd@users.noreply.github.com>
2022-11-23 18:37:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Affordable and Professional Joki Tugas">
+        <meta name="keywords" content="joki tugas, affordable joki tugas, professional joki tugas">
+        <meta name="author" content="Anindya L">
+    <title>JOKI TUGAS | About</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@500&family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap');
+        p {
+            font-family: 'Lexend Exa', sans-serif;
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">NUGASKUY</span>  Joki Tugas</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li class="current"><a href="index.html">Home</a></li>
+                    <li class="current"><a href="about.html">About</a></li>
+                    <li class="current"><a href="services.html">Services</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>  
+
+    <main>
+        <section id="newsletter">
+            <div class="container">
+                <h1>Subscribe To Our Newsletter</h1>
+                <form action="welcome.php" method="post">
+                     First Name: <input type="text" name="firstname"><br>
+                     Last Name: <input type="text" name="lastname"><br>
+                     Email: <input type="text" name="email"><br>
+                            <input type="submit">
+                </form>
+            </div>
+        </section>
+
+        <section id="main">
+            <div class="container">
+                <article id="main-col">
+                    <h1 class="page-title">About Us</h1>
+                    <p class="dark">Hola teman-teman nugaskuy semuanya.
+                        NUGASKUY adalah platform Jasa Pengerjaan Tugas dengan harga paling murah, paling cepat, dan paling akurat di Indonesia.
+                        Tim NUGASKUY terdiri atas orang-orang yang  capable dan berpengalaman dibidangnya. 
+                        Kami melayani pembuatan tugas dari jenjang sekolah dasar hingga kuliah.
+                    </p>
+                    <p class="dark">Platform kami berdiri sejak 2005 dengan banyak clients dan cerita. 
+                        Platform ini dibuat dengan tujuam membantu teman-teman semua untuk mengerjakan tugas dengan mudah dan murah.
+                        Tentunya dengan kualitas high end. 
+                    </p>
+                </article>
+
+                <aside id="sidebar">
+                    <div class="dark">
+                        <h1 class="page-title">What We Do</h1>
+                        <p>Kami berspesialisasi di semua bidang. Mulai dari aritmatika tingkat sekolah dasar hingga Persamaan Diferensial Biasa tingkat perkuliahan.
+                            Kami menguasai baik pelajaran Sains maupun Sosial.
+                        </p>
+                    </div>
+                </aside>
+            </div>
+        </section>
+    </main>
+    
+    <footer>
+        <p>NUGASKUY, Copyright &copy;2009</p>
+    </footer>
+</body>
+</html>
